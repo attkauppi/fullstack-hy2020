36a543b22f06b783201b24ed7174d0f971d33461
attkauppi <ari.kauppi@helsinki.fi>
2020-01-28 05:06:51 +0200

    t1.3

--- osa1/kurssitiedot/src/index.js
+++ osa1/kurssitiedot/src/index.js
@@ -4,6 +4,10 @@ import ReactDOM from 'react-dom';
 //import App from './App';
 //import * as serviceWorker from './serviceWorker';
 
+
+
+// Tehtavan 1.3. sait tehtyä. Sitten lopettelit, koska et uskaltatnut
+// tyontaa githubiin tehtavia, jotka olit tehnyt, mutta joita et ollut merkinnyt.
 const Header = (props) => {
     return (
         <h1>{props.name}</h1>
@@ -19,39 +23,66 @@ const Part = (props) => {
 }
 
 const Content = (props) => {
+    console.log('props value is ', props)
+
+    const parts = props.taulukko;
+
+    const listItems = parts.map((part) =>
+        <Part name={part.name} ex={part.exercises} />
+        //<p>{part.name} {part.exercises}</p>
+    );
+
     return (
-        <div>       
-            <Part name={props.name1} ex={props.ex1} />
-            <Part name={props.name2} ex={props.ex2} />
-            <Part name={props.name3} ex={props.ex3} />
+        
+        <div>
+            {listItems}
         </div>
     )
 }
 
 const Total = (props) => {
+    const number = props
+    console.log('number: ', props)
     return (
         <p>Number of exercises {props.number}</p>
     )
 }
 
-
-
-
 const App = () => {
     const course = 'Half Stack application development'
-    const part1 = 'Fundamentals of React'
-    const exercises1 = 10
-    const part2 = 'Using props to pass data'
-    const exercises2 = 7
-    const part3 = 'State of a component'
-    const exercises3 = 14
 
-   /*  const eka = {
-        name: part1,
-        exercises: exercises1
+    /* const part1 = 
+
+    const part1 = 
+    const exercises1 = 
+    const part2 = 
+    const exercises2 = 
+    const part3 = 
+    const exercises3 = 14 */
+
+    const part1 = {
+        name: 'Fundamentals of React',
+        exercises: 10
     }
 
-    const toka = {
+    const part2 = {
+        name: 'Using props to pass data',
+        exercises: 7
+    }
+
+    const part3 = {
+        name: 'State of a component',
+        exercises: 14
+    }
+
+    const t = [part1, part2, part3]
+
+    const number = part1.exercises + part2.exercises + part3.exercises;
+    console.log('number2: ', number)
+
+    
+
+    /* const toka = {
         name: part2,
         exercises: exercises2
     }
@@ -59,15 +90,16 @@ const App = () => {
     const kolmas = {
         name: part3,
         exercises: exercises3
-    } */
-
+    }
+ */
 
   
     return (
       <div>
         <Header name={course} />
-        <Content name1={part1} ex1={exercises1} name2={part2} ex2={exercises2} name3={part3} ex3={exercises3}/>
-        <Total number={exercises1 + exercises2 + exercises3} />
+        {/* <Content name1={part1} ex1={exercises1} name2={part2} ex2={exercises2} name3={part3} ex3={exercises3}/> */}
+        <Content taulukko = {t}/>
+        <Total number={number} />
       </div>
     )
   }
